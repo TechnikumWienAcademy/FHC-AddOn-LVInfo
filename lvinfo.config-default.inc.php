fc682464af42c19f31934cc74004e895ae49ab9c
Andreas Österreicher <oesi@technikum-wien.at>
2017-12-20 18:07:21 +0100

    Bei der Vorrückung von LV-Infos kann jetzt definiert werden ob diese die alte Freigabe übernehmen sollen oder ob diese auf "In Bearbeitung" gesetzt werden nach dem Vorrücken

--- lvinfo.config-default.inc.php
+++ lvinfo.config-default.inc.php
@@ -1,4 +1,20 @@
 <?php
+/**
+ * Array mit Sprachen die bei den LV-Infos zur Verfügung stehen
+ * DEFAULT: array('German','English')
+ */
 $config_lvinfo_sprachen = array('German','English');
-define('ADDON_LVINFO_SEND_FREIGABEMAIL',true);
+
+/**
+ * Gibt an ob beim Abschicken der LVInfo ein Freigabemail verschickt wird
+ * DEFAULT: true
+ */
+define('ADDON_LVINFO_SEND_FREIGABEMAIL', true);
+
+/**
+ * Gibt an ob beim Vorrücken die neuen LV-Infos bereits freigegeben sind wenn diese bereits freigegeben waren
+ * oder ob diese auf "in Bearbeitung" gesetzt werden
+ * DEFAULT: false
+ */
+define('ADDON_LVINFO_VORRUECKUNG_FREIGABE_UEBERNEHMEN', false);
 ?>
--- vilesci/vorrueckung.php
+++ vilesci/vorrueckung.php
@@ -28,6 +28,7 @@ require_once('../../../include/organisationseinheit.class.php');
 require_once('../../../include/lehrveranstaltung.class.php');
 require_once('../../../include/studienplan.class.php');
 require_once('../include/lvinfo.class.php');
+require_once('../lvinfo.config.inc.php');
 
 echo '<!DOCTYPE HTML>
 <html>
@@ -59,7 +60,6 @@ $db = new basis_db();
 $studiensemester_kurzbz_from = (isset($_POST['studiensemester_kurzbz_from'])?$_POST['studiensemester_kurzbz_from']:'');
 $studiensemester_kurzbz_to = (isset($_POST['studiensemester_kurzbz_to'])?$_POST['studiensemester_kurzbz_to']:'');
 $studiengang_kz = (isset($_POST['studiengang_kz'])?$_POST['studiengang_kz']:'');
-$anzahl_kopiert = 0;
 
 if($studiensemester_kurzbz_from == '')
 {
@@ -72,9 +72,24 @@ if($studiensemester_kurzbz_to == '')
 	$studiensemester_kurzbz_to = $stsem->jump($studiensemester_kurzbz_from, 2);
 }
 
-echo '<form action="vorrueckung.php" method="POST">
+echo 'Diese Seite kopiert die LV-Informationen von einem Semester ins nächste.<br>
+Dabei werden nur die LV-Informationen kopiert die laut Studienplan im Ziel vorhanden sind.
+Bereits bestehende Informationen werden nicht überschrieben.<br>';
+
+if(defined('ADDON_LVINFO_VORRUECKUNG_FREIGABE_UEBERNEHMEN')
+	&& ADDON_LVINFO_VORRUECKUNG_FREIGABE_UEBERNEHMEN === true)
+{
+	echo '<br>Der Freigabestatus wird von der Quelle übernommen.';
+}
+else
+{
+	echo '<br>Die vorgerückten LV-Informationen werden auf "in Bearbeitung" gesetzt.';
+}
+
+echo '<br><br><form action="vorrueckung.php" method="POST">
 Studiengang:
-<select name="studiengang_kz">';
+<select name="studiengang_kz">
+<option value="">Alle</option>';
 
 $stg = new studiengang();
 $stg->getAll('typ, kurzbz');
@@ -127,8 +142,29 @@ echo '</select>';
 echo '<input type="submit" value="Vorruecken" name="copy" />';
 echo '</form>';
 
-if(isset($_POST['copy']) && $studiengang_kz != '' && $studiensemester_kurzbz_from != '' && $studiensemester_kurzbz_to != '')
+if (isset($_POST['copy']) && $studiensemester_kurzbz_from != '' && $studiensemester_kurzbz_to != '')
 {
+	if ($studiengang_kz != '')
+	{
+		// Einzelner Studiengang
+		copyLVInfo($uid, $studiengang_kz, $studiensemester_kurzbz_from, $studiensemester_kurzbz_to);
+	}
+	else
+	{
+		// Alle Studiengaenge
+		foreach($stg->result as $row)
+		{
+			copyLVInfo($uid, $row->studiengang_kz, $studiensemester_kurzbz_from, $studiensemester_kurzbz_to);
+		}
+	}
+}
+
+echo '</body>
+</html>';
+
+function copyLVInfo($uid, $studiengang_kz, $studiensemester_kurzbz_from, $studiensemester_kurzbz_to)
+{
+	$anzahl_kopiert = 0;
 	echo '<br><br>
 			Kopiere LV-Informationen von Studiengang '.$studiengang_kz.'
 			von Studiensemester '.$studiensemester_kurzbz_from.' nach '.$studiensemester_kurzbz_to;
@@ -173,8 +209,21 @@ if(isset($_POST['copy']) && $studiengang_kz != '' && $studiensemester_kurzbz_fro
 
 					if($lvinfo_neu->save())
 					{
+						$lvinfo_alt_status = new lvinfo();
+						$lvinfo_alt_status->getLastStatus($row_lvinfo->lvinfo_id);
+						if ( defined('ADDON_LVINFO_VORRUECKUNG_FREIGABE_UEBERNEHMEN')
+							&& ADDON_LVINFO_VORRUECKUNG_FREIGABE_UEBERNEHMEN === true)
+						{
+							if ($lvinfo_alt_status->lvinfostatus_kurzbz == 'freigegeben')
+								$status_neu = 'freigegeben';
+							else
+								$status_neu = 'bearbeitung';
+						}
+						else
+							$status_neu = 'bearbeitung';
+
 						$lvinfo_neu_status = new lvinfo();
-						if($lvinfo_neu_status->setStatus($lvinfo_neu->lvinfo_id, 'bearbeitung', $uid))
+						if($lvinfo_neu_status->setStatus($lvinfo_neu->lvinfo_id, $status_neu, $uid))
 						{
 							echo ' <span class="ok">OK</span>';
 						}
@@ -197,6 +246,3 @@ if(isset($_POST['copy']) && $studiengang_kz != '' && $studiensemester_kurzbz_fro
 	}
 	echo '<br><br>Kopierte Einträge:'.$anzahl_kopiert;
 }
-
-echo '</body>
-</html>';
