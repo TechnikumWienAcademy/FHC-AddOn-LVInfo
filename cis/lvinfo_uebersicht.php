68422e6a3459c060e6da047eac28da453707e67c
Paolo <bison@technikum-wien.at>
2022-08-19 12:36:10 +0200

    Composer changes

--- cis/lvinfo_uebersicht.php
+++ cis/lvinfo_uebersicht.php
@@ -80,7 +80,7 @@ $datum_obj = new datum();
 	<link rel="stylesheet" href="../../../skin/style.css.php" type="text/css">
 	<link rel="stylesheet" href="../../../skin/tablesort.css" type="text/css">
 
-	<script type="text/javascript" src="../../../vendor/jquery/jqueryV1/jquery-1.12.4.min.js"></script>
+	<script type="text/javascript" src="../../../vendor/jquery/jquery1/jquery-1.12.4.min.js"></script>
 	<script type="text/javascript" src="../../../vendor/christianbach/tablesorter/jquery.tablesorter.min.js"></script>
 
 	<script type="text/javascript">
